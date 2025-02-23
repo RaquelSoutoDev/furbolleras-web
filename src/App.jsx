d0e18891651575f88cf2de9b48038b2727d53fcd
Raquel Souto <raquelsouto@MacBook-Air-de-Raquel.local>
2025-02-23 19:38:44 +0100

    Hasta el papo de la tipo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/base.css';
-import './assets/fonts/DESTROY_.ttf';
+import './assets/fonts/DESTROY_.TFF'
 import Footer from './components/Footer';
 
 //Importaciones de la HOME
